4015ac5fc5b6f208570180c1000301eb5e912d19
Gilangeka00 <gilangekayanda@amikom.ac.id>
2023-03-27 20:56:39 +0700

    tugas pertemuan 3

---
+++ pengenalan class dan object/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace pengenalan_class_dan_object
+{
+    public class Mobil
+    {
+        public string Warna { get; set; }
+        public int JumlahPintu { get; set; }
+        public string Merek { get; set; }
+        public string Model { get; set; }
+        public int TahunKeluaran { get; set; }
+
+        public void InfoMobil()
+        {
+            Console.WriteLine("Warna: {0}", Warna);
+            Console.WriteLine("Jumlah Pintu: {0}", JumlahPintu);
+            Console.WriteLine("Merek: {0}", Merek);
+            Console.WriteLine("Model: {0}", Model);
+            Console.WriteLine("Tahun Keluaran: {0}", TahunKeluaran);
+        }
+        public void Gas(int Kecepatan)
+        {
+            Console.WriteLine("\nMobil " + Model + " berjalan dengan kecepatan " + Kecepatan + "\n");
+        }
+        public void Klakson(string Suara)
+        {
+            Console.WriteLine("{0}\n", Suara);
+        }
+        public void TampilanInfo()
+        {
+            Console.WriteLine("Mobil saya berwarna " + Warna + ", merek " + Merek + ", model " + Model + " keluaran tahun " + TahunKeluaran + " dengan jumlah pintu sebanyak " + JumlahPintu + "");
+        }
+    }
+}
---
+++ pengenalan class dan object/muncul.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pengenalan_class_dan_object
+{
+    internal class Program
+    {
+        class program
+        {
+            static void Main(string[] args)
+            {
+                Mobil mobil1 = new Mobil();
+                mobil1.Warna = "Putih";
+                mobil1.JumlahPintu = 4;
+                mobil1.Merek = "Mitsubishi";
+                mobil1.Model = "Xpander";
+                mobil1.TahunKeluaran = 2017;
+
+                mobil1.InfoMobil();
+                mobil1.Gas(170);
+                mobil1.Klakson("Tut-tut-tut");
+                mobil1.TampilanInfo();
+            }
+        }
+
+    }
+}
